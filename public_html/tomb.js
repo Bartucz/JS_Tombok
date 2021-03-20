f7fd5215be9a01c94f155482ace0ad45573e0b1b
Bartucz <43059630+Bartucz@users.noreply.github.com>
2021-03-20 10:10:44 +0100

    Initial commit

---
+++ public_html/tomb.js
@@ -0,0 +1,74 @@
+
+function ID(nev) {
+    return document.getElementById(nev);
+}
+
+
+
+
+
+var adat="";
+
+function beker() {
+    
+    adat = ID("adat").value;
+}
+var tomb = [];
+
+function dolgozz(){
+   
+     beker();
+     feltolt();
+     osszegzes ();
+     
+     
+    var kiir =ID("kiir");
+    
+    kiir.innerHTML="Jó reggelt <br>"+tomb;
+    
+    var kiir =ID("osszeg");
+    kiir.innerHTML="Összeg "+osszegzes ();
+    
+}
+
+
+function feltolt(){
+    
+    tomb = [];
+    for (var i = 0; i < adat; i++) {
+        tomb[i]=Math.floor(Math.random()*10+1);
+    }
+    
+    
+}
+
+function osszegzes (){
+    var summ = 0;
+    for (var i = 0; i < tomb.length; i++) {
+        summ+= tomb[i];
+    }
+    return summ;
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+function init() {
+   
+    var gomb =ID("gomb");
+    gomb.addEventListener("click",dolgozz);
+    
+    
+    
+}
+
+
+
+window.addEventListener("load", init, false);
