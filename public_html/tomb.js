db05ff3ea5ea1882b1213a3e87042fae62c7c7c3
kotanp <kotanpeter1@gmail.com>
2021-03-23 14:08:59 +0100

    kész projekt

--- public_html/tomb.js
+++ public_html/tomb.js
@@ -22,7 +22,6 @@ function dolgozz() {
     beker();
     feltolt();
     osszegzes();
-    visszafele();
 
 
     var kiir = ID("kiir");
@@ -34,6 +33,30 @@ function dolgozz() {
     
     var kiir=ID("vissza");
     kiir.innerHTML="A tömb visszafele "+visszafele();
+    
+    var kiir=ID("paros");
+    kiir.innerHTML="A páros számok: "+paros();
+    
+    var kiir=ID("ottel");
+    kiir.innerHTML="Öttel osztható: "+otteloszt()+" db";
+    
+    var kiir=ID("max");
+    kiir.innerHTML="A legnagyobb szám: "+legnagyobb();
+    
+    var kiir=ID("kettoharom");
+    kiir.innerHTML="Van-e kettővel és hárommal osztható szám: "+kettoharom();
+    
+    var kiir=ID("negyzetszam");
+    kiir.innerHTML="Van-e négyzetszám: "+negyzetsz();
+    
+    var kiir=ID("negyzetossz");
+    kiir.innerHTML="A négyzetszámok összege: "+negyzetosszeg();
+    
+    var kiir=ID("prim");
+    kiir.innerHTML="Van-e prímszám: "+primszam();
+    
+    var kiir=ID("primdb");
+    kiir.innerHTML="A prímszámok darabszáma: "+primszamdb();
 
 }
 function getRndInteger(min, max) {
@@ -87,5 +110,103 @@ function visszafele(){
     return /*vissza*/ revtomb;
 }
 
+function paros(){
+    var parosok=[];
+    for (var i = 0; i < tomb.length; i++) {
+        if (tomb[i]%2===0) {
+            parosok.push(tomb[i]);
+        }
+    }
+    return parosok;
+}
 
+function otteloszt(){
+    var osztok=0;
+    for (var i = 0; i < tomb.length; i++) {
+        if (tomb[i]%5===0) {
+            osztok++;
+        }
+    }
+    return osztok;
+}
+
+function legnagyobb(){
+    var max=0;
+    for (var i = 0; i < tomb.length; i++) {
+        if (tomb[i]>tomb[max]) {
+            max=i;
+        }
+    }
+    return tomb[max];
+}
+
+function kettoharom(){
+    var vane=false;
+    var i=0;
+    while(i<tomb.length && vane===false){
+        if (tomb[i]%2===0 && tomb[i]%3===0) {
+            vane=true;
+        }
+        i++;
+    }
+    return (vane?"van":"nincs");
+}
+
+function negyzetsz(){
+    var vane=false;
+    var i=0;
+    while(i<tomb.length && vane===false){
+        if (Math.sqrt(tomb[i])%1===0) {
+            vane=true;
+        }
+        i++;
+    }
+    return (vane?"van":"nincs");
+}
+
+function negyzetosszeg(){
+    var osszeg=0;
+    for (var i = 0; i < tomb.length; i++) {
+        if (Math.sqrt(tomb[i])%1===0) {
+            osszeg+=tomb[i];
+        }
+    }
+    return osszeg;
+}
+
+function primszam(){
+    var vane=false;
+    var i=0;
+    var osztok=0;
+    while(i<tomb.length && vane===false){
+        for (var j = 1; j <= tomb[i]; j++) {
+            if (tomb[i]%j===0) {
+                osztok++;
+            }
+        }
+        if (osztok===2) {
+            vane=true;
+        }
+        osztok=0;
+        i++;
+    }
+    return (vane?"van":"nincs");
+}
+
+function primszamdb(){
+    var db=0;
+    var osztok=0;
+    for (var i = 0; i < tomb.length; i++) {
+        for (var j = 1; j <= tomb[i]; j++) {
+            if (tomb[i]%j===0) {
+                osztok++;
+            }
+        }
+        if (osztok===2) {
+            db++;
+        }
+        osztok=0;
+    }
+    return db;
+}
 window.addEventListener("load", init, false);
